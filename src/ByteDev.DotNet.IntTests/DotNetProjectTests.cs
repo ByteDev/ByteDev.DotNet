fad82c58e5c75b5db049c8ee681b2982a70e925b
Paul Stanley <paul.stanley@asos.com>
2018-07-24 15:22:39 +0100

    Added .NET Core int test

--- src/ByteDev.DotNet.IntTests/DotNetProjectTests.cs
+++ src/ByteDev.DotNet.IntTests/DotNetProjectTests.cs
@@ -6,10 +6,14 @@ namespace ByteDev.DotNet.IntTests
     [TestFixture]
     public class DotNetProjectTests
     {
+        private const string TestProjStd20 = @"TestProjs\std20.xml";
+        private const string TestProjCore21 = @"TestProjs\core21.xml";
+        private const string TestProjFramework462 = @"TestProjs\framework462.xml";
+
         [Test]
         public void WhenProjectXmlIsStandard_ThenReturnTargetFramework()
         {
-            var xDocument = XDocument.Load(@"TestProjs\std20.xml");
+            var xDocument = XDocument.Load(TestProjStd20);
 
             var sut = new DotNetProject(xDocument);
 
@@ -17,9 +21,19 @@ namespace ByteDev.DotNet.IntTests
         }
 
         [Test]
+        public void WhenProjectXmlIsCore_ThenReturnTargetFramework()
+        {
+            var xDocument = XDocument.Load(TestProjCore21);
+
+            var sut = new DotNetProject(xDocument);
+
+            Assert.That(sut.TargetFramework, Is.EqualTo("netcoreapp2.1"));
+        }
+
+        [Test]
         public void WhenProjectXmlIsFramework_ThenReturnTargetFramework()
         {
-            var xDocument = XDocument.Load(@"TestProjs\framework462.xml");
+            var xDocument = XDocument.Load(TestProjFramework462);
 
             var sut = new DotNetProject(xDocument);
 
