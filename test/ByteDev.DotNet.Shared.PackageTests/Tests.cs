b60ae273cb3c5b5451a9331756f66abab868a7df
Paul Stanley <paulstanley@protonmail.com>
2020-03-19 11:54:25 +0800

    Fixed package tests

--- test/ByteDev.DotNet.Shared.PackageTests/Tests.cs
+++ test/ByteDev.DotNet.Shared.PackageTests/Tests.cs
@@ -9,9 +9,11 @@ namespace ByteDev.DotNet.Shared.PackageTests
         [Test]
         public void WhenSlnExists_ThenRead()
         {
-            var sln = DotNetSolution.Load(@"Test.sln");
+            var sln = new DotNetSolution(SlnText);
 
             Assert.That(sln.MajorVersion, Is.EqualTo(16));
         }
+
+        private const string SlnText = "Microsoft Visual Studio Solution File, Format Version 12.00\r\n# Visual Studio Version 16\r\nVisualStudioVersion = 16.0.29709.97\r\nMinimumVisualStudioVersion = 10.0.40219.1\r\n";
     }
 }
