ce63ba8dcf5945542e68b4d1fbd404d598c448e9
Paul Stanley <paulstanley@protonmail.com>
2020-03-20 10:23:30 +0800

    Improvements in FormatVersionParser. Added unit tests

--- src/ByteDev.DotNet/Solution/Parsers/FormatVersionParser.cs
+++ src/ByteDev.DotNet/Solution/Parsers/FormatVersionParser.cs
@@ -5,22 +5,27 @@ namespace ByteDev.DotNet.Solution.Parsers
 {
     internal class FormatVersionParser : ISolutionTextParser<string>
     {
-        private const string ErrorMessage = "A valid Format Version could not be found in the sln text.";
+        private const string ExMessage = "A valid Format Version could not be found in the sln text.";
 
         public string Parse(string slnText)
         {
+            if(string.IsNullOrEmpty(slnText))
+                ParserExThrower.ThrowSlnTextNullOrEmpty(nameof(slnText));
+
             try
             {
-                var value = Regex.Match(slnText, "[\r\n]?Microsoft Visual Studio Solution File, Format Version ([0-9.]+)", RegexOptions.Multiline).Groups[1].Value;
+                var match = Regex.Match(slnText, "^Microsoft Visual Studio Solution File, Format Version ([0-9.]+)", RegexOptions.Multiline);
+
+                var value = match.Groups[1].Value;
 
                 if (string.IsNullOrEmpty(value))
-                    throw new InvalidDotNetSolutionException(ErrorMessage);
+                    throw new InvalidDotNetSolutionException(ExMessage);
 
                 return value;
             }
             catch (Exception ex)
             {
-                throw new InvalidDotNetSolutionException(ErrorMessage, ex);
+                throw new InvalidDotNetSolutionException(ExMessage, ex);
             }
         }
     }
---
+++ test/ByteDev.DotNet.UnitTests/Solution/Parsers/FormatVersionParserTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ByteDev.DotNet.Solution;
+using ByteDev.DotNet.Solution.Parsers;
+using NUnit.Framework;
+
+namespace ByteDev.DotNet.UnitTests.Solution.Parsers
+{
+    [TestFixture]
+    public class FormatVersionParserTests
+    {
+        private FormatVersionParser _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new FormatVersionParser();
+        }
+
+        [Test]
+        public void WhenIsNull_ThenThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => _sut.Parse(null));
+        }
+
+        [Test]
+        public void WhenIsEmpty_ThenThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => _sut.Parse(string.Empty));
+        }
+
+        [Test]
+        public void WhenDoesNotContainMsVsSlnFileFormatVersion_ThenThrowException()
+        {
+            Assert.Throws<InvalidDotNetSolutionException>(() => _sut.Parse("Format Version 123"));
+        }
+
+        [Test]
+        public void WhenLineDoesNotStartWithMsVsSlnFileFormatVersion_ThenThrowException()
+        {
+            Assert.Throws<InvalidDotNetSolutionException>(() => _sut.Parse(" Microsoft Visual Studio Solution File, Format Version 12.00"));
+        }
+
+        [TestCase("Microsoft Visual Studio Solution File, Format Version 12.00", "12.00")]
+        [TestCase("\nMicrosoft Visual Studio Solution File, Format Version 12.00", "12.00")]
+        [TestCase("Microsoft Visual Studio Solution File, Format Version 1", "1")]
+        public void WhenContainsMinVsVersion_ThenReturnVersion(string slnText, string expected)
+        {
+            var result = _sut.Parse(slnText);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
